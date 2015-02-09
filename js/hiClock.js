618f7b9cc62aaf2e83827fbcd62677b0486aa68b
castrolol <lu4n.k4str0@gmail.com>
2015-02-09 14:40:36 -0300

    removing self's pattern

--- js/hiClock.js
+++ js/hiClock.js
@@ -1,10 +1,9 @@
   (function() {
 
-   
+
 
     function HiClock(hiClockElement) {
-      var self = this;
-
+      
       this.listeners = [];
 
       this.hour = null;
@@ -13,11 +12,53 @@
       this.inputElement = hiClockElement.querySelector("input");
       this.clockElement = hiClockElement.querySelector(".hi-clock-container");
 
-      [].forEach.call(hiClockElement.classList, function(c){ 
-        self.clockElement.classList.add(c);
-      });
+
+      [].forEach.call(hiClockElement.classList, (function(c) {
+        this.clockElement.classList.add(c);
+      }).bind(this));
 
       this.init();
+
+    };
+
+    HiClock.prototype.onshow = function(cb) {
+      this.listeners.push(cb);
+    }
+
+    HiClock.prototype.setPart = function(ctx) {
+
+      this[ctx.type] = ctx.value;
+      
+      var texto = ("0" + ctx.value).substr(-2, 2);
+      
+      var timeElement = this.clockElement.querySelector(".h-time");
+
+      timeElement.querySelector(".h-" + ctx.type).textContent = texto;
+      
+      this.inputElement.value = ("" + timeElement.textContent).trim();
+
+      var typeElement = this.clockElement.querySelector(".h-numbers.h-" + ctx.type);
+      
+      var active = typeElement.querySelector(".active");
+
+      if (active) active.classList.remove("active");
+
+      [].forEach.call(typeElement.querySelectorAll(".el"), function(el) {
+        if (el.textContent == ctx.value) el.classList.add("active");
+      });
+
+    };
+
+    HiClock.prototype.init = function() {
+      var self = this;
+
+      this.hiClockElement.removeChild(this.clockElement);
+      document.body.appendChild(this.clockElement);
+
+      this.clockElement.classList.remove("min");
+      this.clockElement.classList.remove("hr");
+
+      this.setInitialListeners();
 
       this.setPart({
         type: "hour",
@@ -28,71 +69,47 @@
         value: 0
       });
 
-    };
-
-    HiClock.prototype.onshow = function(cb){
-      this.listeners.push(cb);
     }
 
-    HiClock.prototype.setPart = function(ctx) {
-      this[ctx.type] = ctx.value;
-      var texto = ("0" + ctx.value).substr(-2, 2);
+    HiClock.prototype.setInitialListeners = function() {
+
+      this.inputElement.addEventListener("click", (function() {
+        this.show();
+      }).bind(this));
+
       var timeElement = this.clockElement.querySelector(".h-time");
-    
-      timeElement.querySelector(".h-" + ctx.type).textContent = texto;
-      this.inputElement.value = (""+timeElement.textContent).trim();
-    
-      var typeElement = this.clockElement.querySelector(".h-numbers.h-" + ctx.type);
-      var active = typeElement.querySelector(".active");
-    
-      if( active ) active.classList.remove("active");
-    
-      [].forEach.call(typeElement.querySelectorAll(".el"), function(el){
-        if( el.textContent == ctx.value) el.classList.add("active");
-      });
+      timeElement
+        .querySelector(".h-min")
+        .addEventListener("click", (function() {
+          this.clockElement.classList.add("min");
+          this.clockElement.classList.remove("hr");
+        }).bind(this));
 
-    };
-
-    HiClock.prototype.init = function() {
-      var self = this;
-     
-      this.hiClockElement.removeChild(this.clockElement);
-      document.body.appendChild(this.clockElement);
-
-      this.clockElement.classList.remove("min");
-      this.clockElement.classList.remove("hr");
-      this.inputElement.addEventListener("click", function(){
-        self.show();
-      });
-
-       var timeElement = this.clockElement.querySelector(".h-time");
-       timeElement.querySelector(".h-min").addEventListener("click", function(){
-          self.clockElement.classList.add("min");
-          self.clockElement.classList.remove("hr");
-       });
-
-       timeElement.querySelector(".h-hour").addEventListener("click", function(){
+      timeElement
+        .querySelector(".h-hour")
+        .addEventListener("click", (function() {
           self.clockElement.classList.remove("min");
           self.clockElement.classList.add("hr");
-       });
+        }).bind(this));
 
-      this.clockElement.addEventListener("click", function(e) {
+      this.clockElement.addEventListener("click", (function(e) {
         if (e.target.classList.contains("el")) {
           var parentNode = e.target.parentNode;
           var ctx = {
             type: parentNode.classList.contains("h-min") ? "min" : "hour",
             value: +(e.target.textContent)
           };
-          self.handleAction(ctx);
-        }else if(e.target == self.clockElement){
-          self.hide();
+          this.handleAction(ctx);
+        } else if (e.target == self.clockElement) {
+          this.hide();
         }
-      });
+      }).bind(this));
 
-    }
 
-    
-    HiClock.prototype.setHour = function(v){
+    };
+
+
+    HiClock.prototype.setHour = function(v) {
       this.setPart({
         type: "hour",
         value: v
@@ -100,8 +117,8 @@
       return this;
     };
 
-    
-    HiClock.prototype.setMin = function(v){
+
+    HiClock.prototype.setMin = function(v) {
       this.setPart({
         type: "min",
         value: v
@@ -113,8 +130,7 @@
 
       var value = this.inputElement.value;
       var parts = null;
-      if( parts = value.match(/(2[0-3]|[01][0-9]):([0-5][0-9])/))
-      {
+      if (parts = value.match(/(2[0-3]|[01][0-9]):([0-5][0-9])/)) {
         this
           .setHour(parts[1])
           .setMin(parts[2]);
@@ -123,10 +139,10 @@
 
       this.clockElement.classList.remove("min");
       this.clockElement.classList.add("show");
-      setTimeout((function(){
+      setTimeout((function() {
         this.clockElement.classList.add("hr");
 
-        this.listeners.forEach(function(cb){
+        this.listeners.forEach(function(cb) {
           cb();
         });
       }).bind(this), 10);
@@ -137,11 +153,9 @@
 
       this.clockElement.classList.remove("hr");
       this.clockElement.classList.remove("min");
-      setTimeout((function(){
+      setTimeout((function() {
         this.clockElement.classList.remove("show");
       }).bind(this), 200);
-      
-      
 
     };
 
@@ -163,4 +177,4 @@
 
     window.HiClock = HiClock;
 
-  }()); 
+  }());
