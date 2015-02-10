16492cc305d91a9251345d30ae7b637bd7d78dc2
castrolol <lu4n.k4str0@gmail.com>
2015-02-10 15:47:55 -0300

    added set value to jquery plugin

--- jquery/jquery.hiClock.js
+++ jquery/jquery.hiClock.js
@@ -4,22 +4,33 @@
       <div class="h-numbers h-hour"> <span class="el i-0 ">12</span><span class="el i-1">1</span>   <span class="el i-2">2</span>           <span class="el i-3">3</span>                 <span class="el i-4">4</span>           <span class="el i-5">5</span>          <span class="el i-6">6</span>                 <span class="el i-7">7</span>           <span class="el i-8">8</span>        <span class="el i-9">9</span>        <span class="el i-10 h-2-dig">10</span>           <span class="el i-11 h-2-dig">11</span>        <div class="h-inner-clock">          <span class="el i-0 h-2-dig">0</span>          <span class="el i-1  h-2-dig">13</span>             <span class="el i-2  h-2-dig">14</span>             <span class="el i-3  h-2-dig">15</span>           <span class="el i-4 h-2-dig">16</span>             <span class="el i-5 h-2-dig">17</span>            <span class="el i-6 h-2-dig">18</span>           <span class="el i-7 h-2-dig">19</span> \
                    <span class="el i-8 h-2-dig">20</span>           <span class="el i-9 h-2-dig">21</span>          <span class="el i-10 h-2-dig">22</span>             <span class="el i-11 h-2-dig">23</span>        </div>          </div>    </div>     </div></div>';
 
-	jQuery.fn.hiClock = function(){
+	jQuery.fn.hiClock = function(value){
 
 		function emit(el, event, params){
 			return function(){ $(el).trigger(event, params); };
 		}
 
 		this.each(function(){
+
+			var clock = null;
+			if(clock = $(this).data("clock")){
+				if(value instanceof Date){
+					clock.setDate(value);
+				}else{
+					clock.setTime(value);
+				}
+				return;
+			}
+
 			$(this).empty();
 			$(this).append($(htmlTemplate));
-		});
-
-		this.each(function(){
-			var clock = new HiClock(this);
+	 
+			clock = new HiClock(this);
 			clock.on("change", emit(this, "change", [this]));
 			clock.on("show", emit(this, "show", [this]));
 			clock.on("hide", emit(this, "hide", [this]));
+			$(this).data("clock", clock);
+
 		});
 
 		return this;
